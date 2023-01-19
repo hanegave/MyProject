8e5fee46f752d384417e8d44d8225aac53be0f78
Vaibhav <Vaibhav@DESKTOP-F4JT8EC>
2023-01-19 17:59:32 +0530

    waitProgram update

--- src/test/java/WaitProgram/ExplisitWait.java
+++ src/test/java/WaitProgram/ExplisitWait.java
@@ -14,11 +14,16 @@ public class ExplisitWait {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
+		driver.get("https://demo.actitime.com/login.do");
 		
 		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5000));
 		// to wait until title appears
-		wait.until(ExpectedConditions.titleContains("actiTIME"));
+		boolean status=wait.until(ExpectedConditions.titleContains("actiTIME"));
+		if(status)
+			System.out.println("pass");
+		else
+			System.out.println("fail");
+		
 		WebElement username_TF=driver.findElement(By.id("username"));
 		//to wait till username text field appwars
 		wait.until(ExpectedConditions.visibilityOf(username_TF)).sendKeys("admin");
@@ -27,7 +32,8 @@ public class ExplisitWait {
 		//to wait till the passwaord text field appears
 		wait.until(ExpectedConditions.visibilityOf(passward_TF)).sendKeys("manager");
 		
-		WebElement logButton=driver.findElement(By.id("loginButton"))
+		WebElement logButton=driver.findElement(By.id("loginButton"));
+		logButton.click();
 		//wait until login button
 	}
 
